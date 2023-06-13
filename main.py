d8b468c377671c91a4fa457761fe2d843cd170f0
Showtimeeeeexitgit config --global user.email sipovichit@yandex.rugit config --global user.name Showtimeeee <sipovichit@yandex.ru>
2023-06-13 22:48:20 +0300

    new interface, sound settings, requirements

--- main.py
+++ main.py
@@ -1,24 +1,39 @@
 import random
 import time
+from colorama import init, Fore
 from text_in_sound import text_sound
 
 
-# Counter words
-count_words = 10
+# counter words (50sec)
+counter_words = 10
 
-# open txt (17mb)
+# waiting time between words
+time_word = 10
+
+# colorama init
+init(autoreset=True)
+
+
+# open, read txt (17mb)
 with open('russian.txt', 'r') as file:
     words = file.readlines()
     words = [s.strip("\n") for s in words]
 
 
-# random word and text to speech translation
+# random word in console and text to speech translation
 def random_word(words):
-    for i in range(count_words):
+    top_counter = 1
+    for i in range(counter_words):
+        color_text = Fore.BLUE, Fore.GREEN, Fore.RED
+        rand_color_text = random.choice(color_text)
         my_word = random.choice(words)
-        print(my_word)
+        # console interface
+        print(('*' * counter_words) + ' ' + str(top_counter) + ' ' + ('*' * counter_words))
+        print(rand_color_text + my_word)
+        print()
+        top_counter += 1
         text_sound(my_word)
-        time.sleep(10)
+        time.sleep(time_word)
 
 
 random_word(words)
--- text_in_sound.py
+++ text_in_sound.py
@@ -4,5 +4,8 @@ import pyttsx3
 # text to speech translation
 def text_sound(data):
     sound = pyttsx3.init()
+    sound.setProperty('rate', -100)
+    sound.setProperty('voice', 'ru')
+    sound.setProperty('volume', 5.0)
     sound.say(data)
     return sound.runAndWait()
