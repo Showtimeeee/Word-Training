8842c92bfc7dfb08bd38e28701d4102889adb947
Showtimeeeeexitgit config --global user.email sipovichit@yandex.rugit config --global user.name Showtimeeee <sipovichit@yandex.ru>
2023-06-19 13:02:15 +0300

    progress-bar

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import random
 import time
 from colorama import init, Fore
 from text_in_sound import text_sound
+from tqdm import tqdm
 
 
-# counter words (50sec)
-counter_words = 10
+# counter words (8 sec)
+counter_words = 8
 
 # waiting time between words
 time_word = 5
@@ -32,12 +33,13 @@ def random_word(words, top_counter=1):
         print()
         top_counter += 1
         text_sound(my_word)
-        time.sleep(time_word)
+
+        # progress bar
+        for count in tqdm(range(counter_words)):
+            time.sleep(1)
 
 
 random_word(words)
-
-
 
 
 
